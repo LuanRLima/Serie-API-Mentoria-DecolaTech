dffbc73463bc97e5d5e0660fa26aeb6dd4fb869d
LuanRLima <luanrlima9@gmail.com>
2022-01-02 14:47:16 -0300

    Finalizando Mentoria construindo APIRest simples com dotnet

---
+++ DIO.Series.Web/Controllers/SerieControllerAutomatico.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DIO.Series.Web.Controllers
+{
+    public class SerieControllerAutomatico : Controller
+    {
+        // GET: SerieControllerAutomatico
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SerieControllerAutomatico/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SerieControllerAutomatico/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SerieControllerAutomatico/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SerieControllerAutomatico/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SerieControllerAutomatico/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SerieControllerAutomatico/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SerieControllerAutomatico/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DIO.Series.Web/SerieModel.cs
@@ -0,0 +1,31 @@
+﻿using DIO.Series.Classes;
+using DIO.Series.Enum;
+
+namespace DIO.Series.Web
+{
+    public class SerieModel
+    {
+        public int Id { get; set; }
+        public Genero Genero { get; set; }
+        public string Titulo { get; set; }
+        public string Descrição { get; set;}
+        public int Ano { get; set; }
+        public bool Excluido { get; set; }
+
+        public SerieModel(){ }
+        public SerieModel(Serie serie)
+        {
+            Id = serie.retornaId();
+            Genero = serie.retornaGenero();
+            Titulo = serie.retornaTitulo();
+            Ano = serie.retornaAno();
+            Descrição = serie.retornaDescricao();
+
+        }
+        public Serie ToSerie()
+        {
+            return new Serie(Id, Genero, Titulo, Descrição, Ano);
+        }
+
+    }
+}
